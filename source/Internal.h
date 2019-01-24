007bf410eb84b264f7b33c95ed7a0ee9bfcdc3ea
Nosink D <nosink.d@gmail.com>
2019-01-24 16:44:39 +0100

    includes_std changed to Internal. Added definition for platform and OPEN_GL_X_VERSIONS

---
+++ source/Internal.h
@@ -0,0 +1,18 @@
+#ifndef INTERNAL_H
+#define INTERNAL_H
+
+#define __WINDOWS__
+
+#define OPEN_GL_MAJOR_VERSION 3
+#define OPEN_GL_MINOR_VERSION 3
+
+// C++ STD Includes
+#include <string>
+#include <functional>
+#include <vector>
+#include <iostream>
+#include <memory>
+#include <algorithm>
+#include <windows.h>
+
+#endif // INTERNAL_H
