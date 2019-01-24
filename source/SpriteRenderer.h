82a0295c11e858736e1f19b97e5eb73970310b05
Nosink D <nosink.d@gmail.com>
2019-01-24 14:36:27 +0100

    Simple class not implemented for SpriteRenderer added.

---
+++ source/SpriteRenderer.h
@@ -0,0 +1,23 @@
+#ifndef SPRITE_RENDERER_H
+#define SPRITE_RENDERER_H
+
+#include "includes_std.h"
+
+#include "Component.h"
+
+#include "Color.h"
+#include "Vector3.h"
+
+class SpriteRenderer : public Component {
+
+public:
+
+protected:
+
+private:
+
+    Color m_color;
+
+};
+
+#endif // SPRITE_RENDERER_H
