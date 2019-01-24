993ea95b108f61085e44a1a7abd9f09b8fc60758
Nosink D <nosink.d@gmail.com>
2019-01-24 07:16:26 +0100

    Code for testing.

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,30 @@
 
-#include <iostream>
+#include "Director.h"
+
+#include "Demo.h"
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#define DEMO_SCENE 0
 
 int main(){
 
-    std::cout << "Hello World!";
+    Director * sDirector = Director::getSingleton();
+
+    sDirector->addScene(new Demo());
+
+    sDirector->load(DEMO_SCENE);
+
+    while(!sDirector->isExitTime()){
+
+        sDirector->update();
+
+        sDirector->render();
+
+    }
+
+    delete sDirector;
 
     std::cin.clear();
     std::cin.sync();
