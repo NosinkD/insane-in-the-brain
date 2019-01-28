fe56e5b088b4037202276f56faa0b99477620094
Nosink D <nosink.d@gmail.com>
2019-01-28 21:40:41 +0100

    Includes updated for SDL.

--- source/Internal.h
+++ source/Internal.h
@@ -1,9 +1,7 @@
 #ifndef INTERNAL_H
 #define INTERNAL_H
 
-#define __WINDOWS__
-
-#define OPEN_GL_MAJOR_VERSION 3
+#define OPEN_GL_MAJOR_VERSION 4
 #define OPEN_GL_MINOR_VERSION 3
 
 // C++ STD Includes
@@ -12,8 +10,17 @@
 #include <vector>
 #include <iostream>
 #include <memory>
+#include <stdio.h>
 #include <algorithm>
 #include <windows.h>
 #include <fstream>
+
+// SDL & Glew Includes
+#include <SDL2/SDL.h>
+#include <GL/glew.h>
+#include <SDL2/SDL_opengl.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <SDL2/SDL_image.h>
 
 #endif // INTERNAL_H
